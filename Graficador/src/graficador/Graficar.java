bc5c4f002ce5e6ea4baec872545348cf6da38659
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-01-29 15:56:10 -0500

    More

---
+++ Graficador/src/graficador/Graficar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficador;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author camil
+ */
+public class Graficar {
+    
+    public static ArrayList<Equation> ParseEquation( String Equation ){
+        return null;
+    }
+    public static ArrayList<Par> calc(ArrayList<Equation> eq){
+        return null;
+    } 
+}
