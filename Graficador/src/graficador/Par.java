395f00f688ff4d6d7f1b518a4993c65891be8064
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-01-29 22:36:20 -0500

    Linal polynom works

---
+++ Graficador/src/graficador/Par.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficador;
+
+/**
+ *
+ * @author camil
+ */
+public class Par {
+    double x;
+    double y;
+
+    public Par(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+}
